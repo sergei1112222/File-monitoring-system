9c3e9d2f1da1e1cc01fcb591d837147b3160cd92
Бухонов Сергей <sbuhonov@milane.local>
2018-08-02 13:40:24 +0300

    norm

---
+++ FileMonitoringSystem/Monitoring/Monitor/Monitor.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace FileMonitoringSystem.Monitoring.Monitor
+{
+    public static class Monitor
+    {
+        private static FileSystemWatcher _fileSystemWatcher;
+
+        public static void MonitorNode(string Name)
+        {
+            _fileSystemWatcher = new FileSystemWatcher();
+
+        }
+
+    }
+}
